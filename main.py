93a6f5e8826e7dc063b476e8a15450e045bb9e97
Пользователь <gorenochka79@gmail.com>
2022-11-09 13:42:05 +0200

    Docstrings are added

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
+    """Count damage."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику, равный '
                 f'{5 + randint(3, 5)}')
     if char_class == 'mage':
         return (f'{char_name} нанёс урон противнику, равный '
                 f'{5 + randint(5, 10)}')
-    if char_class == 'healer' :
+    if char_class == 'healer':
         return (f'{char_name} нанёс урон противнику, равный '
                 f'{5 + randint(-3, -1)}')
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Count blocked damage."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -23,6 +26,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Count special skill."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f' «Выносливость {80 + 25}»')
@@ -33,6 +37,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Refer to players with advice."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +62,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Make a choice."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -79,17 +85,18 @@ def choice_char_class() -> str:
             'чтобы выбрать другого персонажа ').lower()
     return char_class
 
-
-def main():
+if __name__ == '__main__':
+    run_screensaver() 
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
-          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+    
 
 
 main()
